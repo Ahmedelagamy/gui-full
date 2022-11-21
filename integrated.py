564220b62f4a19ae0a0c294de7c630c54358f810
Ahmed Mohsen El Agamy <58571185+Ahmedelagamy@users.noreply.github.com>
2022-11-21 09:40:03 +0200

    Update integrated.py

--- integrated.py
+++ integrated.py
@@ -406,6 +406,22 @@ def clean_text(dataframe, col_name):
 bad_reviews_data = clean_text(bad_reviews, 'Comment')
 good_reviews_data= clean_text(good_reviews, 'Comment')
 
+# ngram
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+stoplist = stopwords.words('english') + ['though']
+
+c_vec = CountVectorizer(stop_words=stoplist, ngram_range=(2,3))
+# matrix of ngrams
+ngrams = c_vec.fit_transform(good_reviews_data)
+# count frequency of ngrams
+count_values = ngrams.toarray().sum(axis=0)
+# list of ngrams
+vocab = c_vec.vocabulary_
+df_ngram = pd.DataFrame(sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)
+            ).rename(columns={0: 'frequency', 1:'bigram/trigram'})
+st.write('Top pros')
+st.write(df_ngram)
 tab = st.sidebar.selectbox('Pick one', ['Positive Review', 'Negative Review'])
 
 # Insert containers separated into tabs:
@@ -421,7 +437,7 @@ topic_model = BERTopic(language= 'en',umap_model=umap_model, n_gram_range= (2,3)
 if tab == 'Positive Review':
     
   st.subheader('Positive Reviews')
-  st.dataframe(good_reviews_data)
+ 
     
 # Fixing small dataset bug
   if len(good_reviews) < 200: # Workaround if not enough documents https://github.com/MaartenGr/BERTopic/issues/97 , https://github.com/MaartenGr/Concept/issues/5
